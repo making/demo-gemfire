866cc6a020de5bd833f559136efd27d0e0ab0119
Toshiaki Maki <makingx@gmail.com>
2025-06-17 10:19:44 +0900

    Tweak

--- src/main/java/am/ik/demo/GemfireConfig.java
+++ src/main/java/am/ik/demo/GemfireConfig.java
@@ -1,6 +1,5 @@
 package am.ik.demo;
 
-import java.util.Properties;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
@@ -13,9 +12,7 @@ public class GemfireConfig {
 
 	@Bean
 	ClientCache clientCache(GemfireProps props) {
-		Properties properties = new Properties();
-		properties.setProperty("log-level", props.logLevel());
-		ClientCacheFactory cacheFactory = new ClientCacheFactory(properties);
+		ClientCacheFactory cacheFactory = new ClientCacheFactory().set("log-level", props.logLevel());
 		for (var locator : props.locators()) {
 			cacheFactory.addPoolLocator(locator.host(), locator.port());
 		}
